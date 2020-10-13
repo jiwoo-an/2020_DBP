df0faa1eaa022016c57e1ff6ad512615cb525dca
jiwoo-an <70886768+jiwoo-an@users.noreply.github.com>
2020-10-13 22:56:24 +0900

    Create emp_select.php

---
+++ W06-P/emp_select.php
@@ -0,0 +1,59 @@
+<?php
+    $link = mysqli_connect("localhost", "admin", "admin", "employees");
+    $filtered_emp_num = mysqli_real_escape_string($link, $_GET['emp_num']);
+    $query = "SELECT * FROM employees ORDER BY emp_no DESC LIMIT ".$filtered_emp_num;
+    $result = mysqli_query($link, $query);
+    // print_r($result); 
+    $row = mysqli_fetch_array($result);
+    // print_r($row);
+    $emp_info = '';
+    while($row = mysqli_fetch_array($result)) {
+        $emp_info .= '<tr>';
+        $emp_info .= '<td>'.$row['emp_no'].'</td>'; 
+        $emp_info .= '<td>'.$row['birth_date'].'</td>'; 
+        $emp_info .= '<td>'.$row['first_name'].'</td>'; 
+        $emp_info .= '<td>'.$row['last_name'].'</td>'; 
+        $emp_info .= '<td>'.$row['gender'].'</td>'; 
+        $emp_info .= '<td>'.$row['hire_date'].'</td>'; 
+        $emp_info .= '<td><a href="emp_update.php?emp_no='.$row['emp_no'].'">update</a></td>';
+        $emp_info .= '<td><a href="emp_delete.php?emp_no='.$row['emp_no'].'">delete</a></td>';
+        $emp_info .= '<tr>';
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title> 직원 관리 시스템 </title>
+</head>
+
+<body>
+    <h2><a href="index.php">직원 관리 시스템</a> | 직원 정보 조회</h2>
+    
+    <form action="emp_select.php" method="POST">
+        <label>검색하고 싶은 직원의 수
+            <input type="text" name="number" placeholder='1-100'>
+            <input type="submit" value="ok">
+        </label>
+    </form>
+
+    <table border="1">
+        <tr>
+            <th>emp_no</th>
+            <th>birth_date</th>
+            <th>first_name</th>
+            <th>last_name</th>
+            <th>gender</th>
+            <th>hire_date <br>
+                <button onclick="location.href='emp_select_hire_date_asc.php'">ASC</button>
+                <button onclick="location.href='emp_select_hire_date_desc.php'">DESC</button>
+            </th>
+            <th>update</th>
+            <th>delete</th>
+        </tr>
+        <?=$emp_info?>
+    </table>
+</body>
+</html>
